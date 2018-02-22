7ee7b40b12c5562bf97839d32dcbd9aff9e12a66
mostaelectron <mostaelectron@yahoo.fr>
2018-02-22 22:17:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# pyqt5_camera_viewer
+In this example, we demonstrate how to create simple camera viewer using Opencv3 and PyQt5.
+
+If you  need to change ui_main_window.ui with QtDesigner, don't forget to regenerate ui_main_window.py file using this command:  
+```{r, engine='sh', count_lines}
+pyuic5 main_window.ui -o main_window.py
+```
+
+
+
---
+++ ui_main_window.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_window.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(525, 386)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Form)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.image_label = QtWidgets.QLabel(Form)
+        self.image_label.setObjectName("image_label")
+        self.verticalLayout.addWidget(self.image_label)
+        self.control_bt = QtWidgets.QPushButton(Form)
+        self.control_bt.setObjectName("control_bt")
+        self.verticalLayout.addWidget(self.control_bt)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Cam view"))
+        self.image_label.setText(_translate("Form", "TextLabel"))
+        self.control_bt.setText(_translate("Form", "Start"))
